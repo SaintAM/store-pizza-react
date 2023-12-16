dcb77dea4703a0e61b11f769cff9f538c1968e7f
SaintAM <mistersteepstar@gmail.com>
2023-12-16 15:32:03 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./scss/app.scss";
+import Sort from "./components/Sort";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import PizzaBlock from "./components/PizzaBlock";
+import pizzasJson from "./assets/pizzas.json";
+import Skeleton from "./components/PizzaBlock/Skeleton";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("https://64861b03a795d24810b7b7ef.mockapi.io/items")
+            .then((res) => res.json())
+            .then((data) => {
+                setItems(data);
+                setIsLoading(false);
+            });
+    }, []);
+
+    return (
+        <>
+            <div className="wrapper">
+                <Header />
+                <div className="content">
+                    <div className="container">
+                        <div className="content__top">
+                            <Categories />
+                            <Sort />
+                        </div>
+                        <h2 className="content__title">Все пиццы</h2>
+                        <div className="content__items">
+                            {isLoading
+                                ? [...new Array(6)].map((_, i) => (
+                                      <Skeleton key={i} />
+                                  ))
+                                : items.map((obj) => (
+                                      <PizzaBlock key={obj.id} {...obj} />
+                                  ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+];
+
+const Categories = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const onClickCategory = (index) => {
+        setActiveIndex(index);
+    };
+
+    return (
+        <div className="categories">
+            <ul>
+                {categories.map((item, index) => (
+                    <li
+                        key={index}
+                        className={activeIndex === index ? "active" : ""}
+                        onClick={() => onClickCategory(index)}
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Categories;
